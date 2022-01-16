8b6a0dc3c269814f2630d7886e809ef3d89d6481
lukdav <luke-davies@outlook.com>
2022-01-16 22:09:52 +0000

    Initial commit

---
+++ robert_price_bm/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for robert_price_bm project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'robert_price_bm.settings')
+
+application = get_asgi_application()
---
+++ robert_price_bm/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for robert_price_bm project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'robert_price_bm.settings')
+
+application = get_wsgi_application()
