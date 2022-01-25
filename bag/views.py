73eb2de9ce2a504d8c225f11d9d248f07328483b
lukdav <luke-davies@outlook.com>
2022-01-25 11:35:09 +0000

    Added toast styling, messages and functionality

--- bag/views.py
+++ bag/views.py
@@ -25,7 +25,7 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated {size} {product.name} quantity to {bag[item_id]['items_by_size'][size]}')
+                messages.success(request, f'Updated quantity of {size} {product.name} to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
                 messages.success(request, f'Added {size} {product.name} to your shopping bag')
@@ -35,7 +35,7 @@ def add_to_bag(request, item_id):
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(request, f'Updated quantity of {product.name} to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your shopping bag')
@@ -57,7 +57,7 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request, f'Updated {size} {product.name} quantity to {bag[item_id]['items_by_size'][size]}')
+            messages.success(request, f'Updated quantity of {size} {product.name} to {bag[item_id]["items_by_size"][size]}')
 
         else:
             del bag[item_id]['items_by_size'][size]
@@ -67,7 +67,7 @@ def adjust_bag(request, item_id):
     else:
         if quantity > 0:
             bag[item_id] = quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(request, f'Updated quantity of {product.name} to {bag[item_id]}')
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your shopping bag')
