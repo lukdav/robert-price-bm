94cf434bc504a7a75dcd976c2503d75f624b31e2
lukdav <luke-davies@outlook.com>
2022-01-25 21:23:13 +0000

    Added stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,21 @@
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#FF0100',
+        iconColor: '#FF0100'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KFmbgD3SFldYSV5AWapWc9Ik9mupEk0SYhRUN08WfzSyeikQtd1x1Qsv7cFwX8pi5FoYJBDsmr9OGvMEbejv8qF00BDwvoSoz',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
