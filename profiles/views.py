763a96a800691f8ab38c48df9cef689d25413956
lukdav <luke-davies@outlook.com>
2022-01-27 21:34:25 +0000

    Finished add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -16,8 +16,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile successfully updated')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please check the form and try again.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
