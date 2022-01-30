9cc01969cc2da83d68dbd7bc34bab72b52d8d336
lukdav <luke-davies@outlook.com>
2022-01-30 17:22:43 +0000

    News admin - news form

---
+++ news/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+from .models import NewsPost
+
+
+class NewsPostForm(models.ModelForm):
+
+    class Meta:
+        model = NewsPost
+        fields = (
+            'title',
+            'image',
+            'content',
+        )
+
+    image = forms.ImageField(label='Image',
+        required=True, widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Set placeholders, auto-focus to title and a class to the fields
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'image': 'Image',
+            'content': 'Article Content',
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'image':
+                self.fields[field].widget.attrs['placeholder'] = (
+                    placeholders[field])
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 news-form-input'
