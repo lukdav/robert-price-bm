9aca05e01f188be474cc1f50f1778c60cbdb36df
lukdav <luke-davies@outlook.com>
2022-01-30 23:57:04 +0000

    Deploying to new heroku app

--- robert_price_bm/settings.py
+++ robert_price_bm/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['robert-price.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['robert-price-bm.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,23 +121,19 @@ WSGI_APPLICATION = 'robert_price_bm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+    }
 
 
 # Password validation
