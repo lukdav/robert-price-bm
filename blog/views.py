871842295fc8521f98dfd90034284c0c1050718c
lukdav <luke-davies@outlook.com>
2022-01-30 02:51:32 +0000

    Finished setting up blog app

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from .models import Blog
 
 def blog_posts(request):
     """ A view to return the blog """
+    all_posts = Blog.objects.all()
+
     template = 'blog/blog.html'
-    context = {}
+    context = {
+        'all_posts': all_posts,
+    }
 
     return render(request, template, context)
