a5de3d8c1ae7d1451c63eeb5d407a721f5a55710
lukdav <luke-davies@outlook.com>
2022-01-30 14:32:24 +0000

    Issue with affixing slug to blog page

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.blog_posts, name='blog_posts'),
-    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
-]
