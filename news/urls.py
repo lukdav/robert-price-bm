77bbe10ff451db9d5f5e610d7cb60a2abc669146
lukdav <luke-davies@outlook.com>
2022-01-30 16:02:27 +0000

    Added news article page

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_news, name='news'),
+    path('<slug:slug>/', views.news_article, name='news_article'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import NewsPost
 
 
@@ -12,3 +12,15 @@ def all_news(request):
     }
 
     return render(request, 'news/news.html', context)
+
+
+def news_article(request, slug):
+    """ A view to show individual news articles """
+
+    article = get_object_or_404(NewsPost, slug=slug)
+
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'news/news_article.html', context)
