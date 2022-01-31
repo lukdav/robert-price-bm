ea788fc10e6cba2b199e0af044d5c58ba4ef4fb2
lukdav <luke-davies@outlook.com>
2022-01-31 01:11:09 +0000

    Attempt to view products

--- robert_price_bm/settings.py
+++ robert_price_bm/settings.py
@@ -178,6 +178,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
